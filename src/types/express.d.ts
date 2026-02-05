577ff4ed61d78df79117c314dee68a49be986173
Woallex <alexpl.vt100@icloud.com>
2026-02-05 17:50:57 -0300

    chore(config): adjust tsconfig and Express typing

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,6 @@
-import * as express from "express";
-
-declare global {
-  namespace Express {
-    interface Request {
-      userId?: string;
-      userLogin?: string;
-    }
+declare namespace Express {
+  export interface Request {
+    userId?: string;
+    userLogin?: string;
   }
 }
